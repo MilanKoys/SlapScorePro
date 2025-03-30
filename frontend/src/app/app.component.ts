43c4c0b42405eb0b2eed7bf7f302dec0549e5b1a
Milan Koyš <milankoys@tuta.com>
2025-03-30 10:34:19 +0200

    Init frontend

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,94 @@
+import { HttpClient } from '@angular/common/http';
+import {
+  Component,
+  inject,
+  Signal,
+  signal,
+  WritableSignal,
+} from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+interface DiscordUser {
+  accent_color: number;
+  avatar: string;
+  avatar_decoration_data: null;
+  banner: null;
+  banner_color: string;
+  clan: null;
+  collectibles: null;
+  discriminator: string;
+  email: string;
+  flags: number;
+  global_name: string;
+  id: string;
+  locale: string;
+  mfa_enabled: boolean;
+  premium_type: number;
+  primary_guild: null;
+  public_flags: number;
+  username: string;
+  verified: boolean;
+}
+
+export type Nullable<T> = null | T;
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  private readonly _httpClient: HttpClient = inject(HttpClient);
+  private readonly _username: WritableSignal<Nullable<string>> = signal(null);
+  private readonly _avatar: WritableSignal<Nullable<string>> = signal(null);
+  protected readonly username: Signal<Nullable<string>> =
+    this._username.asReadonly();
+  protected readonly avatar: Signal<Nullable<string>> =
+    this._avatar.asReadonly();
+  constructor() {
+    const fragment = new URLSearchParams(window.location.hash.slice(1));
+    let [accessToken, tokenType] = [
+      fragment.get('access_token'),
+      fragment.get('token_type'),
+    ];
+    if (!accessToken || !tokenType) return;
+    localStorage.setItem('token', accessToken);
+    localStorage.setItem('tokenType', tokenType);
+
+    accessToken = localStorage.getItem('token');
+    tokenType = localStorage.getItem('tokenType');
+
+    if (!accessToken || !tokenType) return;
+
+    this._httpClient
+      .get<DiscordUser>('https://discord.com/api/users/@me', {
+        headers: {
+          authorization: `${tokenType} ${accessToken}`,
+        },
+      })
+      .subscribe((response) => {
+        this._username.set(response.global_name);
+        this._avatar.set(
+          `https://cdn.discordapp.com/avatars/${response.id}/${response.avatar}.png`
+        );
+        this._join({
+          avatar: response.avatar,
+          email: response.email,
+          id: response.id,
+          username: response.global_name,
+        });
+      });
+  }
+
+  private _join(user: {
+    id: string;
+    avatar: string;
+    email: string;
+    username: string;
+  }) {
+    this._httpClient
+      .post('http://localhost:4000/join', user)
+      .subscribe((token) => {});
+  }
+}
---
+++ frontend/src/app/app.config.ts
@@ -0,0 +1,16 @@
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { withNgxsReduxDevtoolsPlugin } from '@ngxs/devtools-plugin';
+import { provideStore } from '@ngxs/store';
+import { provideHttpClient } from '@angular/common/http';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(),
+    provideStore([], withNgxsReduxDevtoolsPlugin()),
+  ],
+};
