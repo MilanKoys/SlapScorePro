f87ea6fcfa69b12cc9d4ea89201887d4cabf352f
redyslav <redy0911@gmail.com>
2025-04-05 17:26:10 +0200

    New font, Pugs and more

--- frontend/src/app/pugs/pugs.component.ts
+++ frontend/src/app/pugs/pugs.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { DropdownSelectComponent } from '../dropdown-select/dropdown-select.component';
 
 @Component({
   selector: 'app-pugs',
-  imports: [],
+  imports: [DropdownSelectComponent],
   templateUrl: './pugs.component.html',
   styleUrl: './pugs.component.scss',
 })
 export class PugsComponent {
   title = 'hocičo to je jedno';
+  avatar = signal('fsa');
 }
