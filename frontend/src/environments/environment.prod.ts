1d4d908cd0a2a36e9db994bbb5a1f588bdd93d13
Milan Koyš <milankoys@tuta.com>
2025-04-05 23:15:08 +0200

    Discord url

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,3 @@
 export const environment = {
-  discordRedirect: 'https://discord.com/oauth2/authorize?client_id=1353396004464099391&response_type=token&redirect_uri=https%3A%2F%2Fserver.buri.tech%3A4200&scope=identify+email+connections'
+  discordRedirect: 'https://discord.com/oauth2/authorize?client_id=1353396004464099391&response_type=code&redirect_uri=http%3A%2F%2Fserver.buri.tech%3A4200&scope=identify+email+connections'
 };
