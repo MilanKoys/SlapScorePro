b57b558d3c8463bacf01885219091309e90dfb42
Milan Koyš <milankoys@tuta.com>
2025-04-06 09:55:27 +0200

    Setup changes

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  api: "http://server.buri.tech:4220/",
-  websocket: "http://server.buri.tech:4250/",
+  api: "http://server.buri.tech:4220",
+  websocket: "http://server.buri.tech:4250",
   discordRedirect: 'https://discord.com/oauth2/authorize?client_id=1353396004464099391&response_type=token&redirect_uri=http%3A%2F%2Fserver.buri.tech%3A4200&scope=identify+email+connections'
 };
